24aa730778f554587711f4153fe80224be0a394e
Kaushal Khokhar <kaushal2413@gmail.com>
2021-06-02 09:29:10 +0530

    Http Updated

--- HttpRequests/src/App.js
+++ HttpRequests/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,11 +8,11 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMovies() {
+  const fetchMovies = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/film");
+      const response = await fetch("https://swapi.dev/api/films");
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
@@ -30,7 +30,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMovies()
+  }, [fetchMovies]);
 
   let content = <p>Found no movies!</p>;
 
@@ -52,6 +56,13 @@ function App() {
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>{content}</section>
+      {/* another alternative for content
+      <section>
+        {!isLoading && movieList.length > 0 && <MoviesList movies={movieList} />}
+        {!isLoading && movieList.length === 0 && !error && <p>Found no movies!</p>}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <p>Loading....</p>}
+      </section> */}
     </React.Fragment>
   );
 }
